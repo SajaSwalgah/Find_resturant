b8326db96f8851b1af1368ce59b1a089a9de6be7
LTUC Student 8 <your email here in single quotes>
2021-01-20 09:12:06 +0200

    Add project files.

---
+++ Find_resturant/Find_resturant/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+//22de543e21b2a5e8a43853736e22ca4c
+namespace Find_resturant
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
